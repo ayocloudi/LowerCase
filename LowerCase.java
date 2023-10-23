4a68ac106ddfd3d859c7e297f6ada8a64c6f7c8e
byAyo <ayomide5462@gmail.com>
2023-10-23 21:57:39 +0100

    Add files via upload
    Converts String to lowercase using the toLowerCase() method, and then prints both the original and lowercase versions to the console.

---
+++ LowerCase.java
@@ -0,0 +1,13 @@
+public class LowerCase {
+  public static void main(String[] args) {
+      // Original string
+      String original = "HeLLo WoRLD!";
+      
+      // Convert to lowercase
+      String lowercase = original.toLowerCase();
+      
+      // Print out the result
+      System.out.println("Original String: " + original);
+      System.out.println("Lowercase String: " + lowercase);
+  }
+}
